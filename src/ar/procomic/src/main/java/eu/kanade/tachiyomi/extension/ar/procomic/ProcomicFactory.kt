cbfd61eb14807eb6bd84f28c6a3f6c967e8a08ad
abedelkaderhachimi120-alt <abedelkaderhachimi120@gmail.com>
2026-06-26 21:39:31 +0100

    Delete src/ar/procomic/src/main/java/eu/kanade/tachiyomi/extension/ar/procomic/ProcomicFactory.kt

--- src/ar/procomic/src/main/java/eu/kanade/tachiyomi/extension/ar/procomic/ProcomicFactory.kt
+++
@@ -1,8 +0,0 @@
-package eu.kanade.tachiyomi.extension.ar.procomic
-
-import eu.kanade.tachiyomi.source.Source
-import eu.kanade.tachiyomi.source.SourceFactory
-
-class ProcomicFactory : SourceFactory {
-    override fun createSources(): List<Source> = listOf(Procomic())
-}

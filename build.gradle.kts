6cdce867c43c9338613dda6b988ec8c8e20308d9
abedelkaderhachimi120-alt <abedelkaderhachimi120@gmail.com>
2026-06-26 21:59:21 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
 
 plugins {
     alias(libs.plugins.android.application) apply false
-  alias(libs.plugins.android.library) apply false 
+    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.serialization) apply false
 
     alias(kei.plugins.spotless)
